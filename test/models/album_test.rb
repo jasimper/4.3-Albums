d9a67b25ff67d305a8f5da1cdd27e9571a9b93f5
Jamie Simper <fromraintohope@gmail.com>
2015-07-08 23:57:23 -0400

    Added validation and tests

--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -1,8 +1,51 @@
 require 'test_helper'
 
 class AlbumTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
 
+  test "title must be present" do
+    a = Album.new(genre: 'Test Genre', artist: 'Test Artist', length: 1)
+    assert_equal a.valid?, false
+  end
+
+  test "artist must be present" do
+    a = Album.new(title: 'Test Title', genre: 'Test Genre', length: 1)
+    assert_equal a.valid?, false
+  end
+
+  test "genre must be present" do
+    a = Album.new(title: 'Test Title', artist: 'Test Artist', length: 1)
+    assert_equal a.valid?, false
+  end
+
+  test "length must be present" do
+    a = Album.new(title: 'Test Title', genre: 'Test Genre', artist: 'Test Artist')
+    assert_equal a.valid?, false
+  end
+
+  test "title length must be greater than 0" do
+    a = Album.new(title: '', genre: 'Test Genre', artist: 'Test Artist', length: 1)
+    assert_equal a.valid?, false
+  end
+
+  test "artist length must be greater than 0" do
+    a = Album.new(title: 'Test Title', genre: 'Test Genre', artist: '', length: 1)
+    assert_equal a.valid?, false
+  end
+
+  test "genre length must be greater than 0" do
+    a = Album.new(title: 'Test Title', genre: '', artist: 'Test Artist', length: 1)
+    assert_equal a.valid?, false
+  end
+
+  test "length length must be greater than 0" do
+    a = Album.new(title: 'Test Title', genre: 'Test Genre', artist: 'Test Artist', length: 0)
+    assert_equal a.valid?, false
+  end
+
+  test "no duplicates of artist and song combo" do
+    a = Album.new(title: 'Test Title', genre: 'Test Genre', artist: 'Test Artist', length: 10)
+    a.save
+    b = Album.new(title: 'Test Title', genre: 'Test_Genre', artist: 'Test Artist', length: 30)
+    assert_equal b.valid?, false
+  end
 end
