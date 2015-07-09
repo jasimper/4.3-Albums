123aa9d0da3cdb8d247c59ba3bce4b11dc1ac901
Jamie Simper <fromraintohope@gmail.com>
2015-07-09 09:42:06 -0400

    refactoring

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,6 +1,5 @@
 class Album < ActiveRecord::Base
   validates :title, :genre, :artist, :length, presence: true
-  validates :title, :genre, :artist, length: { minimum: 1 }
   validates :length, numericality: { greater_than: 0 }
   validates :title, uniqueness: { scope: :artist }
 end
--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -4,7 +4,7 @@ class AlbumTest < ActiveSupport::TestCase
 
   test "create album works" do
     a = Album.new(title: 'Test Title', genre: 'Test Genre', artist: 'Test Artist', length: 1)
-    assert_equal a.valid?, true
+    assert a.valid?
   end
 
   test "save album works" do
@@ -15,48 +15,48 @@ class AlbumTest < ActiveSupport::TestCase
 
   test "title must be present" do
     a = Album.new(genre: 'Test Genre', artist: 'Test Artist', length: 1)
-    assert_equal a.valid?, false
+    refute a.valid?
   end
 
   test "artist must be present" do
     a = Album.new(title: 'Test Title', genre: 'Test Genre', length: 1)
-    assert_equal a.valid?, false
+    refute a.valid?
   end
 
   test "genre must be present" do
     a = Album.new(title: 'Test Title', artist: 'Test Artist', length: 1)
-    assert_equal a.valid?, false
+    refute a.valid?
   end
 
   test "length must be present" do
     a = Album.new(title: 'Test Title', genre: 'Test Genre', artist: 'Test Artist')
-    assert_equal a.valid?, false
+    refute a.valid?
   end
 
   test "title length must be greater than 0" do
     a = Album.new(title: '', genre: 'Test Genre', artist: 'Test Artist', length: 1)
-    assert_equal a.valid?, false
+    refute a.valid?
   end
 
   test "artist length must be greater than 0" do
     a = Album.new(title: 'Test Title', genre: 'Test Genre', artist: '', length: 1)
-    assert_equal a.valid?, false
+    refute a.valid?
   end
 
   test "genre length must be greater than 0" do
     a = Album.new(title: 'Test Title', genre: '', artist: 'Test Artist', length: 1)
-    assert_equal a.valid?, false
+    refute a.valid?
   end
 
   test "length length must be greater than 0" do
     a = Album.new(title: 'Test Title', genre: 'Test Genre', artist: 'Test Artist', length: 0)
-    assert_equal a.valid?, false
+    refute a.valid?
   end
 
   test "no duplicates of artist and song combo" do
     a = Album.new(title: 'Test Title', genre: 'Test Genre', artist: 'Test Artist', length: 10)
     a.save
     b = Album.new(title: 'Test Title', genre: 'Test_Genre', artist: 'Test Artist', length: 30)
-    assert_equal b.valid?, false
+    refute b.valid?
   end
 end
